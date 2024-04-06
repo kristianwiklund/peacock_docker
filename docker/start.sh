d893b4db2c8239987c68d4348e297e6fcf6a35c5
Kristian Wiklund <githubkristian@snabela.nl>
2024-04-06 11:33:28 +0200

    added backup of player data (to tar file) on container start

--- docker/start.sh
+++ docker/start.sh
@@ -33,7 +33,7 @@ if [ ! -d /Peacock/.git ]; then
     info_message "Pulling Peacock from github"
     git pull origin master
 else
-    
+    info_message "Checking for Peacock updates"
     cd /Peacock; git pull origin master
 fi
 
@@ -41,6 +41,27 @@ fi
 
 info_message "Building the server"
 cd /Peacock && yarn install && yarn build
+
+# Install contracts
+#info_message "Installing/Updating contracts"
+#git config --global --add safe.directory /Contracts
+
+#if [ ! -d /Contracts ]; then
+#    git clone https://github.com/thepeacockproject/Contracts /Contracts
+#else
+#    cd /Contracts
+#    git pull
+#fi
+
+#info_message "Running rsync to update contracts in server data directory"
+#rsync -av /Contracts/contracts/h3/ /Peacock/contracts/
+
+info_message "Running backup of player metadata"
+mkdir -p /Peacock/backups
+name=userdata_$(date '+%Y-%m-%d_%H%M')
+cd /Peacock && tar zcvf /Peacock/backups/$name.tgz userdata
+
+
 
 info_message "Starting Peacock"
 node /Peacock/chunk0.js
